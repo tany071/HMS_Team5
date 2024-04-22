aa4f2f2d10b6cf01ffd6ea9f32b752dff5d6cbec
aahlaad26 <aahlaad26092002@gmail.com>
2024-04-22 11:16:45 +0530

    created files for doctor

---
+++ HMS-Team 5/Views/Doctor/PrescriptionFormView.swift
@@ -0,0 +1,18 @@
+//
+//  PrescriptionFormView.swift
+//  HMS-Team 5
+//
+//  Created by user3 on 22/04/24.
+//
+
+import SwiftUI
+
+struct PrescriptionFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrescriptionFormView()
+}
